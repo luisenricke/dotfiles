86c0e73ca105e339affbd114b18a50501c0ad5c6
Luis Villalobos <luisevm24@outlook.com>
2020-11-05 02:49:03 -0600

    split the fonts scripts

---
+++ install/font.sh
@@ -0,0 +1,46 @@
+# helper functions
+download_font_from_1001fonts() {
+    local url=$1
+    local output_file=$2
+    local output_directory=$3
+    
+    if [ ! -d /usr/local/share/fonts/$output_directory ]; then
+        echo -e "\n Installing '$output_directory' font \n"
+        curl $url -L -o $output_file
+        unzip $output_file "*.ttf" -d $output_directory
+        rm $output_file
+        sudo mv $output_directory /usr/local/share/fonts
+    fi
+}
+
+sudo apt install curl -y
+sudo apt install unzip -y
+
+# Fira Code
+if [ ! -d /usr/local/share/fonts/FiraCode ]; then
+    echo -e "\n Installing 'FiraCode' font \n"
+    
+    FIRA_CODE_VERSION="5.2"
+    FIRA_CODE_FILE="Fira_Code_v$FIRA_CODE_VERSION.zip"
+
+    curl https://github.com/tonsky/FiraCode/releases/download/$FIRA_CODE_VERSION/$FIRA_CODE_FILE -L -o $FIRA_CODE_FILE
+    unzip $FIRA_CODE_FILE "ttf/*"
+    rm $FIRA_CODE_FILE
+    mv ttf FiraCode
+    sudo mv FiraCode/ /usr/local/share/fonts
+
+    unset FIRA_CODE_VERSION
+    unset FIRA_CODE_FILE
+fi
+
+# Anonymous Pro
+download_font_from_1001fonts "https://dl.1001fonts.com/anonymous-pro.zip" "anonymous-pro.zip" "AnonymousPro"
+
+# Ariesta Moon Demo
+download_font_from_1001fonts "https://dl.1001fonts.com/ariesta-moon-demo.zip" "ariesta-moon-demo.zip" "AriestaMoonDemo"
+
+
+# reference
+# 
+# * https://vitux.com/how-to-install-custom-fonts-in-debian/
+# * https://bash.cyberciti.biz/guide/Pass_arguments_into_a_function
