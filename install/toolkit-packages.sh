356fc750c3bfae75d0543bba168f787bfd111a86
Luis Villalobos <luisevm24@outlook.com>
2020-11-09 18:34:54 -0600

    split packages into file

---
+++ install/toolkit-packages.sh
@@ -0,0 +1,23 @@
+# IMPORTANT 
+#   * enable 'Non-DFSG-compatible Software (non-free)'
+
+sudo apt install synaptic -y
+
+sudo apt install build-essential -y
+sudo apt install dkms -y
+sudo apt install linux-headers-$(uname -r) -y
+
+sudo apt install apt-transport-https -y
+sudo apt install ca-certificates -y
+sudo apt install software-properties-common -y
+
+sudo apt install font-manager -y
+
+sudo apt install zip -y
+sudo apt install unzip -y
+sudo apt install rar -y
+sudo apt install unrar -y
+
+sudo apt install curl -y
+
+sudo apt install vlc -y
