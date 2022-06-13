a10b653fb16d52e64986aa3eb8bbb5d52ed6a211
Luis Villalobos <luisevm24@outlook.com>
2022-06-12 22:32:55 -0500

    update aliases in powershell

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -10,3 +10,34 @@ Set-PSReadLineOption -EditMode Windows
 Import-Module z
 
 Import-Module posh-git
+
+function toBack {
+    cd ..
+}
+Set-Alias .. toBack
+
+function toSecondLastBack {
+    cd ..\..
+}
+Set-Alias ..\.. toSecondLastBack
+
+function toThirdLastBack {
+    cd ..\..\..
+}
+Set-Alias ..\..\.. toThirdLastBack
+
+function mysqlConsole {
+    cmd /c "$ENV:ProgramFiles\MySQL\MySQL Server 8.0\bin\mysql" -u root -p
+}
+Set-Alias mysql_ mysqlConsole
+
+function postgresqlConsole {
+    cmd /c "$ENV:ProgramFiles\PostgreSQL\14\bin\psql"  -U postgres -h localhost -p 5432
+}
+Set-Alias psql_ postgresqlConsole
+
+function artisanCommand($arg1,$arg2,$arg3,$arg4,$arg5) {
+    php artisan $arg1 $arg2 $arg3 $arg4 $arg5
+}
+
+Set-Alias phpa artisanCommand
